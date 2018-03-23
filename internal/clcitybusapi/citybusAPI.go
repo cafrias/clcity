24fc3d3c4d23b91ad727103499bd355e2f5d5d0a
friasdesign <carlos.a.frias@gmail.com>
2018-03-23 16:13:33 -0300

    Add services interfaces

--- internal/clcitybusapi/citybusAPI.go
+++ internal/clcitybusapi/citybusAPI.go
@@ -1,6 +1,6 @@
 package clcitybusapi
 
-// Parada representa una parada como es usado por Cuando Llega.
+// Parada represents a 'Parada' as used by app 'Cuando Llega City Bus'.
 type Parada struct {
 	Codigo                     string `json:"Codigo"`
 	Identificador              string `json:"Identificador"`
@@ -12,10 +12,20 @@ type Parada struct {
 	LongitudParadaGIT          string `json:"LongitudParadaGIT"`
 }
 
-// Linea representa una linea como es usada por Cuando Llega.
+// Linea represents a 'Linea' as used by app 'Cuando Llega City Bus'.
 type Linea struct {
 	CodigoLineaParada string `json:"CodigoLineaParada"`
 	Descripcion       string `json:"Descripcion"`
 	CodigoEntidad     string `json:"CodigoEntidad"`
 	CodigoEmpresa     string `json:"CodigoEmpresa"`
+}
+
+// ParadaService has actions to fetch 'Parada' data from Cuando Llega City Bus API.
+type ParadaService interface {
+	ParadasPorLinea(CodigoLineaParada string) []*Parada
+}
+
+// LineaService has actions to fetch 'Linea' data from Cuando Llega City Bus API.
+type LineaService interface {
+	LineasPorEmpresa(CodigoEmpresa string) []*Linea
 }
